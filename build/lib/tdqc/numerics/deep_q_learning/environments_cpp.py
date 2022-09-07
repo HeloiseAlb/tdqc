56e7b63be4d1e74412675358daaf74d4e426f0b4
H.Albot <H.Albot@physik.uni-muenchen.de>
2022-09-07 11:08:13 +0200

    Cancelling the forcing of the density matrices to be positive definite

--- build/lib/tdqc/numerics/deep_q_learning/environments_cpp.py
+++ build/lib/tdqc/numerics/deep_q_learning/environments_cpp.py
@@ -258,46 +258,6 @@ class DynamicalEvolution(QuantumEnv):
     def __init__(self, **other_params):
         super().__init__(calculate_target_state=True,
                          **other_params)
-    def __isPD(self,B):
-        """Returns true when input is positive-definite, via Cholesky"""
-        try:
-            _ = linalg.cholesky(B)
-            return True
-        except linalg.LinAlgError:
-            return False
-
-    def __nearestPD(self,A):
-        """Find the nearest positive-definite matrix to input
-        A Python/Numpy port of John D'Errico's `nearestSPD` MATLAB code [1], which
-        credits [2].
-        [1] https://www.mathworks.com/matlabcentral/fileexchange/42885-nearestspd
-        [2] N.J. Higham, "Computing a nearest symmetric positive semidefinite
-        matrix" (1988): https://doi.org/10.1016/0024-3795(88)90223-6
-        """
-        B = (A + A.T) / 2
-        _, s, V = linalg.svd(B)
-        H = np.dot(V.T, np.dot(np.diag(s), V))
-        A2 = (B + H) / 2
-        A3 = (A2 + A2.T) / 2
-        if self.__isPD(A3):
-            return A3
-        spacing = np.spacing(linalg.norm(A))
-        # The above is different from [1]. It appears that MATLAB's `chol` Cholesky
-        # decomposition will accept matrixes with exactly 0-eigenvalue, whereas
-        # Numpy's will not. So where [1] uses `eps(mineig)` (where `eps` is Matlab
-        # for `np.spacing`), we use the above definition. CAVEAT: our `spacing`
-        # will be much larger than [1]'s `eps(mineig)`, since `mineig` is usually on
-        # the order of 1e-16, and `eps(1e-16)` is on the order of 1e-34, whereas
-        # `spacing` will, for Gaussian random matrixes of small dimension, be on
-        # othe order of 1e-16. In practice, both ways converge, as the unit test
-        # below suggests.
-        I = np.eye(A.shape[0])
-        k = 1
-        while not self.__isPD(A3):
-            mineig = np.min(np.real(linalg.eigvals(A3)))
-            A3 += I * (-mineig * k**2 + spacing)
-            k += 1
-        return A3
 
     def reward(self,action_sequence,rho_target):        
         # Return the reward of the action_sequence computed according to rho_target.
@@ -309,7 +269,6 @@ class DynamicalEvolution(QuantumEnv):
         rho_DQS = np.tensordot(np.conjugate(final_state), final_state, axes=0)        
 
         # The density matrix is a normalized, positive definite matrix.
-        rho_DQS = self.__nearestPD(rho_DQS)
         trace_rho_DQS = np.trace(rho_DQS)
         if trace_rho_DQS != 0:
             rho_DQS = rho_DQS/trace_rho_DQS # Normalizaton of the final state. 
--- build/lib/tdqc/numerics/ed/models_ed.py
+++ build/lib/tdqc/numerics/ed/models_ed.py
@@ -150,7 +150,6 @@ class State(object):
     def get_density_matrix(self):
         self._density_mat = np.tensordot(np.conjugate(self.vec_state), self.vec_state, axes=0)
         # The function returns a normalized, positive definite matrix.
-        self._density_mat = self.__nearestPD(self._density_mat)
         trace_density_mat = np.trace(self._density_mat)
         if trace_density_mat != 0:
             self._density_mat = self._density_mat/trace_density_mat
@@ -160,43 +159,4 @@ class State(object):
     def class_method(cls):
         return cls, "is class of mathematical models of quantum systems composed of two-level subsystems."    
 
-    def __isPD(self,B):
-        """Returns true when input is positive-definite, via Cholesky"""
-        try:
-            _ = linalg.cholesky(B)
-            return True
-        except linalg.LinAlgError:
-            return False
-
-    def __nearestPD(self,A):
-        """Find the nearest positive-definite matrix to input
-        A Python/Numpy port of John D'Errico's `nearestSPD` MATLAB code [1], which
-        credits [2].
-        [1] https://www.mathworks.com/matlabcentral/fileexchange/42885-nearestspd
-        [2] N.J. Higham, "Computing a nearest symmetric positive semidefinite
-        matrix" (1988): https://doi.org/10.1016/0024-3795(88)90223-6
-        """
-        B = (A + A.T) / 2
-        _, s, V = linalg.svd(B)
-        H = np.dot(V.T, np.dot(np.diag(s), V))
-        A2 = (B + H) / 2
-        A3 = (A2 + A2.T) / 2
-        if self.__isPD(A3):
-            return A3
-        spacing = np.spacing(linalg.norm(A))
-        # The above is different from [1]. It appears that MATLAB's `chol` Cholesky
-        # decomposition will accept matrixes with exactly 0-eigenvalue, whereas
-        # Numpy's will not. So where [1] uses `eps(mineig)` (where `eps` is Matlab
-        # for `np.spacing`), we use the above definition. CAVEAT: our `spacing`
-        # will be much larger than [1]'s `eps(mineig)`, since `mineig` is usually on
-        # the order of 1e-16, and `eps(1e-16)` is on the order of 1e-34, whereas
-        # `spacing` will, for Gaussian random matrixes of small dimension, be on
-        # othe order of 1e-16. In practice, both ways converge, as the unit test
-        # below suggests.
-        I = np.eye(A.shape[0])
-        k = 1
-        while not self.__isPD(A3):
-            mineig = np.min(np.real(linalg.eigvals(A3)))
-            A3 += I * (-mineig * k**2 + spacing)
-            k += 1
-        return A3
+    
